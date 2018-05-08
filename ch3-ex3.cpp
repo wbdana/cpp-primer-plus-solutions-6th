b5f0a0962954c0a7469f8297318134e5dea23e6c
wbdana <william.b.dana@gmail.com>
2018-05-07 22:11:45 -0400

    Add ch3-ex3.cpp

---
+++ ch3-ex3.cpp
@@ -0,0 +1,26 @@
+// chapter 3 exercise 3
+// enter latitude in degrees, minutes, seconds
+// display latitude in decimal format
+#include <iostream>
+int main()
+{
+    using namespace std;
+    const int Minutes_Factor = 60;     // 60 minutes per degree
+    const int Seconds_Factor = 60;     // 60 seconds per minute
+
+    int degrees, minutes, seconds;
+    cout << "Enter a latitude in degrees, minutes, and seconds:" << endl;
+    cout << "First, enter the degrees: ___\b\b\b";
+    cin >> degrees;
+    cout << "Next, enter the minutes of arc: ___\b\b\b";
+    cin >> minutes;
+    cout << "Finally, enter the seconds of arc: ___\b\b\b";
+    cin >> seconds;
+    double additional_minutes = double(minutes) + (double(seconds) / double(Seconds_Factor));
+    double additional_degrees = additional_minutes / double(Minutes_Factor);
+    // double result = 
+    cout << additional_minutes << endl;
+    cout << additional_degrees << endl;
+
+    return 0;
+}
